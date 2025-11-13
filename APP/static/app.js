2d2020e13b372e6a04f4ad8a05b9e8c41f13154b
TONY65391 <tonyanthony65391@gmail.com>
2025-11-12 18:48:58 -0600

    second commit

--- APP/static/app.js
+++ APP/static/app.js
@@ -2,3 +2,9 @@ const API_KEY = `05d5c4c8e843e9277522fcbd6dbc07ea`;
 const container = document.querySelector('.container');
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
+
+
+const nav = document.querySelector('header nav');
+nav.onclick = () => {
+    nav.classList.toggle('open');
+}
